bc19743f028b09067d55e96b6af4e1938c10f6cc
Alejandro Mery <amery@jpi.io>
2023-04-01 02:11:58 +0100

    go-import: export URLAsGoImport returning the go get path if ?go-get=1 is present
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- src/packages/go-import/src/index.ts
+++ src/packages/go-import/src/index.ts
@@ -1,1 +1,16 @@
+export interface GoImport {
+	package: string;
+}
+
+export function URLAsGoImport(url: string): GoImport | null {
+	const { hostname, pathname, searchParams: qs } = new URL(url);
+	if (qs.get("go-get") === "1") {
+		const pkg = `${hostname}${pathname}`;
+		return {
+			package: pkg.endsWith("/") ? pkg.slice(0, -1) : pkg,
+		};
+	}
+	return null;
+}
+
 export default {};
