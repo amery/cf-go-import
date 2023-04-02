82efd46a21f02c137321b4c887697be894aa7a55
Alejandro Mery <amery@jpi.io>
2023-04-02 01:18:23 +0100

    cf-go-import: remove boilerplate
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- src/apps/cf-go-import/src/index.ts
+++ src/apps/cf-go-import/src/index.ts
@@ -1,24 +1,7 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 import * as go from "@licensly/go-import";
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
+	NAMESPACE: KVNamespace;
 }
 
 async function goGetHandler(pkg: go.GoImport, env: Env): Promise<Response> {
@@ -36,7 +19,7 @@ async function requestHandler(request: Request, env: Env): Promise<Response> {
 	}
 
 	// pass through
-	return fetch(request)
+	return fetch(request);
 }
 
 const worker: ExportedHandler<Env> = {
